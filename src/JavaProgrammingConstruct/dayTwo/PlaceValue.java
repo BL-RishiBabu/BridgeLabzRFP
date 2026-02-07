25ce3669329b549ba9f4602cadd8b9308c97fef0
Rishi Babu <rishibabu.rajesh@bridgelabz.com>
2026-02-07 12:51:02 +0530

    [Rishi Babu] added Place value program to read 1, 10, 100 or 1000 to display it is Unit, Ten, Hundred or Thousand.

---
+++ src/JavaProgrammingConstruct/dayTwo/PlaceValue.java
@@ -0,0 +1,23 @@
+package JavaProgrammingConstruct.dayTwo;
+
+import java.util.Scanner;
+
+public class PlaceValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number (1, 10, 100, or 1000) : ");
+        int number = sc.nextInt();
+        if (number == 1) {
+            System.out.println("Unit");
+        } else if (number == 10) {
+            System.out.println("Ten");
+        } else if (number == 100) {
+            System.out.println("Hundred");
+        } else if (number == 1000) {
+            System.out.println("Thousand");
+        } else {
+            System.out.println("Not a valid input for this program");
+        }
+        sc.close();
+    }
+}
